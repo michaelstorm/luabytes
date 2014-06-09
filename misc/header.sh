c6a9b66f2938288c3a8cae041d292d7f32a11f68
Michael Storm <mstorm7@gmail.com>
2014-06-08 22:08:54 -0700

    Support different headers for different file types in check-headers script

--- misc/header.sh
+++ misc/header.sh
@@ -12,27 +12,3 @@
 # 
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-source_dirs="demo lib misc"
-source_exts="cpp h sh"
-header_path="misc/header"
-
-missing=0
-
-for ext in $source_exts; do
-    lines=`wc -l ${header_path}.${ext} | awk '{print $1;}'`
-    header=`cat ${header_path}.${ext}`
-    for f in `find $source_dirs -name *.$ext`; do
-        file_header=`head -$lines $f`
-        if [ "$header" != "$file_header" ]; then
-            echo "HEADER MISSING (add from $header_path.$ext): $f"
-            missing=1
-        fi
-    done
-done
-
-if [ "$missing" -eq 1 ]; then
-    exit 1
-else
-    exit 0
-fi
